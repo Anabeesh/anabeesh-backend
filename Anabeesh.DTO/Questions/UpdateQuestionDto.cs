55e48e3fbd7cb05cc7957d2b3218b1ee372fa8b7
amr-ali <devamrali@gmail.com>
2018-01-19 14:21:34 +0200

    questions list service

---
+++ Anabeesh.DTO/Questions/UpdateQuestionDto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anabeesh.DTO.Questions
+{
+    public class UpdateQuestionDto
+    {
+        /// <summary>
+        /// Question Id
+        /// </summary>
+        [Required]
+        public long Id { get; set; }
+
+        /// <summary>
+        /// Question headline
+        /// </summary>
+        [Required]
+        [MaxLength(200)]
+        public string Headline { get; set; }
+
+        /// <summary>
+        /// Question description
+        /// </summary>
+        [Required]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Number of upvotes of the question
+        /// </summary>
+        [Required]
+        public long Upvotes { get; set; }
+
+    }
+}
